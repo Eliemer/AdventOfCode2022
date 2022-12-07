b0c4b5dbf3338c94e85aca7db0024b15f866af72
Eliemer <eliemer619@gmail.com>
2022-12-06 20:08:54 -0500

    Day 6!! Cleaaan!!!

---
+++ Day 6/Tuning Trouble.fsx
@@ -0,0 +1,23 @@
+open System.IO
+
+let inputStr = File.ReadLines "input.txt"
+
+let MARKER_SIZE = 4
+
+// Part 1
+inputStr
+|> Seq.exactlyOne
+|> Seq.windowed MARKER_SIZE
+|> Seq.findIndex (fun arr -> arr.Length = (Array.distinct arr).Length)
+|> (+) MARKER_SIZE
+|> printfn "Num of chars until start-of-packet Marker was detected: %d"
+
+let MARKER_SIZE' = 14
+
+// Part 2
+inputStr
+|> Seq.exactlyOne
+|> Seq.windowed MARKER_SIZE'
+|> Seq.findIndex (fun arr -> arr.Length = (Array.distinct arr).Length)
+|> (+) MARKER_SIZE'
+|> printfn "Num of chars until start-of-packet Marker was detected: %d"
